8b906b701deda019b21f49ed0996d104c8fe76e2
fuadhaider <mohaiderfuad@gmail.com>
2019-12-13 23:33:46 +0000

    swipe control soted

--- Assets/Scripts/SubmarineMove.cs
+++ Assets/Scripts/SubmarineMove.cs
@@ -6,12 +6,13 @@ public class SubmarineMove : MonoBehaviour {
 
 	bool dead = false;
 	Rigidbody2D rb;
-	public float maxTime;
-	float startTime;
-	float endTime;
+	// public float maxTime;	//maximun allowed swipe time
+	// float startTime;
+	// float endTime;
 	float swipeTime;
 	Vector2 startPos;
 	Vector2 endPos;
+	bool swipeStarted = false;
 	float swipeDis;
 	public float swipeSpeed;
 	public AudioClip deadSound;
@@ -23,68 +24,74 @@ public class SubmarineMove : MonoBehaviour {
 		playerAS = GetComponent<AudioSource>();
 	}
 
-    void Update()
+  void Update()
+  {
+		// if (Input.GetMouseButtonDown(0)) {
+    //     startTime = Time.time;
+    //     startPos = Input.mousePosition;
+		// }
+		// else if (Input.GetMouseButtonUp(0)) {
+    //     startTime = Time.time;
+    //     endPos = Input.mousePosition;
+		// 
+    //     swipeDis = (endPos - startPos).magnitude;
+    //     swipeTime = endTime - startTime;
+		// 
+    //     if (swipeTime < maxTime)
+    //     {
+    //         swipe();
+    //     }
+		// }
+    if (Input.touchCount > 0 && !dead)
     {
-			// if (Input.GetMouseButtonDown(0)) {
-	    //     startTime = Time.time;
-	    //     startPos = Input.mousePosition;
-			// }
-			// else if (Input.GetMouseButtonUp(0)) {
-	    //     startTime = Time.time;
-	    //     endPos = Input.mousePosition;
-			// 
-	    //     swipeDis = (endPos - startPos).magnitude;
-      //     swipeTime = endTime - startTime;
-			// 
-      //     if (swipeTime < maxTime)
-      //     {
-      //         swipe();
-      //     }
-			// }
-        if (Input.touchCount > 0 && !dead)
-        {
-            Touch touch = Input.GetTouch(0);
-				
-            if (touch.phase == TouchPhase.Began)
-            {  //first touch
-                startTime = Time.time;
-                startPos = touch.position;
-				
-            }
-            else if (touch.phase == TouchPhase.Moved)
-            {  //latest moved position
-                endTime = Time.time;
-                endPos = touch.position;
-				
-                swipeDis = (endPos - startPos).magnitude;
-                swipeTime = endTime - startTime;
-				
-                if (swipeTime < maxTime)
-                {
-                    swipe();
-                }
-            }
-            else if (touch.phase == TouchPhase.Ended)
-            {  //finger removed
-                endTime = Time.time;
-                endPos = touch.position;
-				
-                swipeDis = (endPos - startPos).magnitude;
-                swipeTime = endTime - startTime;
-				
-                if (swipeTime < maxTime)
-                {
-                    swipe();
-                }
-            }
+        Touch touch = Input.GetTouch(0);
+		
+        if (touch.phase == TouchPhase.Began)
+        { //first touch
+          // startTime = Time.time;
+          startPos = touch.position;
+        }
+        else if (touch.phase == TouchPhase.Moved)
+        {  //latest moved position
+            // endTime = Time.time;
+					
+						endPos = touch.position;
+            swipeDis = (endPos - startPos).magnitude;
+						// (a-b).magnitude is the distance between them
+            // swipeTime = endTime - startTime;
+		
+            // if (swipeTime < maxTime)
+            // {
+                swipe();
+            // }
+						startPos = endPos;
+
+        }
+        else if (touch.phase == TouchPhase.Ended)
+        {  //finger removed
+            // endTime = Time.time;
+            endPos = touch.position;
+		
+            swipeDis = (endPos - startPos).magnitude;
+            // swipeTime = endTime - startTime;
+		
+            // if (swipeTime < maxTime)
+            // {
+                swipe();
+            // }
         }
     }
+  }
 		
 	void swipe(){
 		if (endPos.y > startPos.y) {
-			rb.AddForce (new Vector2 (0 , swipeSpeed * swipeDis * Time.deltaTime));//1st
-			//rb.AddForce(transform.up * swipeSpeed*swipeDis*Time.deltaTime);//2nd swipespeed:.05, deltatime:75
-			//rb.velocity = new Vector2 (rb.velocity.x, swipeDis * swipeSpeed*Time.deltaTime);//2nd swipespeed:.05, deltatime:1
+			rb.AddForce (new Vector2 (0 , swipeSpeed * swipeDis * Time.deltaTime));
+			// Time.deltaTime moves item by time rather than by frame.
+			// it is a fragment of a second or the time passed since the last frame.
+			//1st rb.AddForce(transform.up * swipeSpeed*swipeDis*Time.deltaTime);
+			//2nd swipespeed:.05, deltatime:75
+			//rb.velocity = new Vector2 (rb.velocity.x, swipeDis * swipeSpeed*Time.deltaTime);
+			//2nd swipespeed:.05, deltatime:1
 		} 
 		else {
 			rb.AddForce (new Vector2 (0 , -swipeSpeed * swipeDis * Time.deltaTime));
